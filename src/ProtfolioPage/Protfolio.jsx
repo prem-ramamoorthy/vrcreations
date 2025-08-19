1e9bf80205687919e66411ae6de6ab3d7296b1e3
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-08-19 18:56:58 +0530

    made global import of the styles in protfolio page

--- src/ProtfolioPage/Protfolio.jsx
+++ src/ProtfolioPage/Protfolio.jsx
@@ -5,31 +5,11 @@ import ProtfolioSection from './ProtfolioSection'
 import Stats from './Stats'
 import Footer from '../Footer/FooterCommon';
 import { useEffect } from 'react'
+import "/styles/protfolioStyle.css"
+import "/styles/headerStyle.css"
+import "/styles/footerStyle.css"
 
 function Protfolio() {
-
-    useEffect(() => {
-        const link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = "/styles/protfolioStyle.css";
-        document.head.appendChild(link);
-
-        const link2 = document.createElement("link");
-        link2.rel = "stylesheet";
-        link2.href = "/styles/headerStyle.css";
-        document.head.appendChild(link2);
-
-        const link3 = document.createElement("link");
-        link3.rel = "stylesheet";
-        link3.href = "/styles/footerStyle.css";
-        document.head.appendChild(link3);
-
-        return () => {
-            document.head.removeChild(link);
-            document.head.removeChild(link2);
-            document.head.removeChild(link3);
-        };
-    }, []);
 
     return (
         <>
